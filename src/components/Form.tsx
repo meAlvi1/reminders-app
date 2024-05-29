baf7f259aa6b9eb29ac60a84eb9f19ba41b71c06
meAlvi1 <jadoondesigner@gmail.com>
2024-05-30 01:51:53 +0500

    Applied `validation` with `react-hook-form` using its three properities `register - to take input`, `handlesubmit - to send data to server` and `formState :{ errors} - to log errors`.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,17 @@
 import React, { FormEvent, useRef } from 'react'
 import { useForm } from 'react-hook-form';
 
+interface FormData {
+    name: string;
+    age: number;
+}
+
 const Form = () => {
     // using hookform library with less code to get from data
-    const  { register, handleSubmit } = useForm();
+    // register for taking input
+    // handleSubmit for sending data to server
+    // formState for validation, showing errors etc
+    const  { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
     // submit function to server
     const onSubmit = (data: any) => {
@@ -14,7 +22,9 @@ const Form = () => {
     <form onSubmit={handleSubmit(onSubmit)}> 
         <div className="mb-3">
             <label htmlFor="name" className="form-label">Name</label>
-            <input {...register('name')} id="name" type="text" className="form-control" />
+            <input {...register('name', { required: true, minLength: 3})} id="name" type="text" className="form-control" />
+            { errors.name?.type === 'required' && <p className="text-danger">The name field is required.</p>}
+            { errors.name?.type === 'minLength' && <p className="text-danger">The name must be at least 3 characters.</p>}
         </div> 
         <div className="mb-3">
             <label htmlFor="age" className="form-label">age</label>
