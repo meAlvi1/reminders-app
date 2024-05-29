d31e2492facdded82f5e43ce9dcd88b4c6c34aec
meAlvi1 <jadoondesigner@gmail.com>
2024-05-29 22:12:11 +0500

    updating  comments on what does `!isExpanded` do, which is basically negation of current value means if `True` turn into `False` vice versa

--- src/components/ExpandableText.tsx
+++ src/components/ExpandableText.tsx
@@ -12,6 +12,7 @@ const ExpandableText = ({children, maxChars = 100}: Props ) => {
   // to summarize the text
   // here if the text is less than the maxChars then we return the text
   const text = isExpanded ? children: children.substring(0, maxChars) + '...';
+  // !isExpanded is negation of current value of isExpanded, means `if isExpanded is true then it will be false and vice versa`
   return <p>{text}<button onClick={() => setIsExpanded(!isExpanded)}>{isExpanded ? 'Show less' : 'Show more'}</button></p>
 }
 
