523aa4965cd52571b4102cd233c9ed6e5b49e08d
meAlvi1 <jadoondesigner@gmail.com>
2024-05-30 01:23:40 +0500

    added `userRef hook` used `HTMLInputElement` to get data from HTML input fields. in `useRef` `null` gets updated with `value` which is from `html`

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,21 +1,33 @@
-import React, { FormEvent } from 'react'
+import React, { FormEvent, useRef } from 'react'
 
 const Form = () => {
-    // handle form submission, preventDefault means to not send data to server
+
+    // useRef hook is used to store a reference to an element
+    const nameRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null);
+    const person = { name: '', age: 0 };
+
     // when we talking to server, we shouldn't use preventDefault
     const handleSubmit = (event: FormEvent) => {
         event.preventDefault();
-        console.log('Submitted');
+        // here nameRef.current == value (which is html element)
+        if (nameRef.current !== null)
+            person.name = nameRef.current.value;
+        if (ageRef.current !== null)
+            person.age = parseInt(ageRef.current.value);
+        
+        // we get json data for submission to server
+        console.log(person);
     }
   return (
     <form onSubmit={handleSubmit}> 
         <div className="mb-3">
             <label htmlFor="name" className="form-label">Name</label>
-            <input id="name" type="text" className="form-control" />
+            <input ref={nameRef} id="name" type="text" className="form-control" />
         </div> 
         <div className="mb-3">
             <label htmlFor="age" className="form-label">age</label>
-            <input id="age" type="number" className="form-control" />
+            <input ref={ageRef} id="age" type="number" className="form-control" />
         </div>
         <button className="btn btn-primary" type="submit">Submit</button>     
     </form>
